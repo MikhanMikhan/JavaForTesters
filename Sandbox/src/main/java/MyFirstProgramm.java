ea2623cce8755b5abe6b63caa3acef889210ad13
MikhanMikhan <kabelecsup@gmail.com>
2019-11-17 01:25:06 +0700

    Сделал функцию расчета расстояния

--- Sandbox/src/main/java/MyFirstProgramm.java
+++ Sandbox/src/main/java/MyFirstProgramm.java
@@ -1,5 +1,12 @@
 public class MyFirstProgramm {
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        Point p1 = new Point(0,0);
+        Point p2 = new Point(4,3);
+
+        System.out.println(p1.distance(p2));
+
+
     }
 }
--- Sandbox/src/main/java/Point.java
+++ Sandbox/src/main/java/Point.java
@@ -1,14 +1,14 @@
 public class Point {
 
-    double a;
-    double b;
+    public double a;
+    public double b;
 
-    public Point (double a, double b){
+    public Point (int a, int b){
         this.a=a;
         this.b=b;
     }
 
-//    public double distance() {
-//        return this.a-this.b;
-//    }
+    public double distance (Point p2) {
+        return Math.sqrt((this.a-p2.a)*(this.a-p2.a)+(this.b-p2.b)*(this.b-p2.b));
+    }
 }
